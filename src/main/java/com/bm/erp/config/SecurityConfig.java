32ccf42493f898c16ab47dbfab5bee8b6640dd4e
Thiago Duszczak <thiagodus@gmail.com>
2026-07-07 18:24:57 -0300

    addded organization controller and v1 securtity config

---
+++ src/main/java/com/bm/erp/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.bm.erp.config;
+
+import com.bm.erp.organization.service.OrganizationService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
+}
--- src/main/java/com/bm/erp/organization/controller/OrganizationController.java
+++ src/main/java/com/bm/erp/organization/controller/OrganizationController.java
@@ -5,6 +5,7 @@ import com.bm.erp.organization.model.dto.OrganizationRequest;
 import com.bm.erp.organization.model.dto.OrganizationResponse;
 import com.bm.erp.organization.service.OrganizationService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,13 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/organization")
+@RequiredArgsConstructor
 public class OrganizationController {
 
-    private OrganizationService organizationService;
-
-    public OrganizationController(OrganizationService organizationService) {
-        this.organizationService = organizationService;
-    }
+    private final OrganizationService organizationService;
 
     @PutMapping
     public ResponseEntity<OrganizationResponse> save(@Valid @RequestBody OrganizationRequest organizationRequest){
